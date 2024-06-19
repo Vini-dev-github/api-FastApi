d2aad422da2b0b15cdef317709e197f5570e664c
Vini <viniciuslima.profissional@gmail.com>
2024-06-19 16:59:19 -0300

    add models

---
+++ contas_a_pagar_e_receber/models/contas_a_pagar_e_receber.py
@@ -0,0 +1,10 @@
+from shared.database import Base
+from sqlalchemy import  Column, Numeric, String, Integer
+
+class ContasPagarReceber(Base):
+    __tablename__ = "contas_a_pagar_e_receber"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    descricao = Column(String(30))
+    valor = Column(Numeric)
+    tipo = Column(String(30))
